53a4e2d8dc766fba6868e19c8c5f0df548b13072
ewqq38 <finntepza@gmail.com>
2026-05-16 21:01:39 +0700

    Add aura generation and visual display functionality

--- script.js
+++ script.js
@@ -1,1 +1,85 @@
+const auraBar = document.getElementById("auraBar");
+    chance: 5
+  },
+  {
+    name: "MYTHIC",
+    color: "hotpink",
+    chance: 1.8
+  },
+  {
+    name: "DIVINE",
+    color: "white",
+    chance: 0.2
+  }
+];
 
+function getRandomAura() {
+  let random = Math.random() * 100;
+  let total = 0;
+
+  for (let aura of auras) {
+    total += aura.chance;
+
+    if (random <= total) {
+      return aura;
+    }
+  }
+}
+
+function generateRollVisuals(finalAura) {
+  auraBar.innerHTML = "";
+
+  for (let i = 0; i < 25; i++) {
+    const aura = auras[Math.floor(Math.random() * auras.length)];
+
+    const div = document.createElement("div");
+    div.className = "aura-card";
+
+    div.textContent = aura.name;
+    div.style.background = aura.color;
+
+    auraBar.appendChild(div);
+  }
+
+  const finalCard = document.createElement("div");
+  finalCard.className = "aura-card";
+  finalCard.textContent = finalAura.name;
+  finalCard.style.background = finalAura.color;
+
+  auraBar.appendChild(finalCard);
+}
+
+rollBtn.addEventListener("click", () => {
+  const finalAura = getRandomAura();
+
+  generateRollVisuals(finalAura);
+
+  auraBar.style.transition = "none";
+  auraBar.style.transform = "translateX(0px)";
+
+  setTimeout(() => {
+    auraBar.style.transition = "transform 4s cubic-bezier(.1,.8,.1,1)";
+    auraBar.style.transform = "translateX(-3900px)";
+  }, 50);
+
+  result.textContent = "ROLLING...";
+
+  setTimeout(() => {
+    result.textContent = "YOU GOT: " + finalAura.name;
+
+    result.style.color = finalAura.color;
+
+    if (
+      finalAura.name === "LEGENDARY" ||
+      finalAura.name === "MYTHIC" ||
+      finalAura.name === "DIVINE"
+    ) {
+      document.body.classList.add("shake");
+      result.classList.add("legendary");
+
+      setTimeout(() => {
+        document.body.classList.remove("shake");
+      }, 600);
+    }
+  }, 4200);
+});
